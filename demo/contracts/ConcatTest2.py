3ce33896b9b41ff90dd8865ba4fad3a3b140d297
Travis <rockacola@gmail.com>
2018-01-09 00:21:22 +1100

    Update doc

--- demo/contracts/ConcatTest2.py
+++ demo/contracts/ConcatTest2.py
@@ -4,12 +4,20 @@ Data Concatenation Test
 A simple utilise contract that attempts to concatenate 2 values together, disregard of their data types.
 
 Test Command:
-    build [FILE_PATH] test 0710 05 True False concat ['hey','ho']
-    build ./demo/contracts/ConcatTest2.py test 0710 05 True False concat ['hey','ho']
-
+    build [FILE_PATH] test 0710 05 False False concat ['hey','ho']
+    build ./demo/contracts/ConcatTest2.py test 0710 05 False False concat ['hey','ho']
+    import contract ./demo/contracts/ConcatTest2.avm 0710 05 False False
 Example Executions:
     testinvoke [CONTRACT_HASH] concat ['lorem','ipsum']
     testinvoke [CONTRACT_HASH] concat ['cloud',9']
+    testinvoke 1c3e9ef16ca1e3927bc663231243a7e0ddd5c818 concat ['lorem','ipsum']
+    testinvoke 1c3e9ef16ca1e3927bc663231243a7e0ddd5c818 concat ['cloud',9]
+    testinvoke 1c3e9ef16ca1e3927bc663231243a7e0ddd5c818 concat ['text',b'1010']
+Invalid Examples:
+    testinvoke [CONTRACT_HASH] concat [true,false]
+    testinvoke [CONTRACT_HASH] concat ['null',null]
+False Positive Examples:
+    testinvoke [CONTRACT_HASH] concat [0.9,1.23]
 """
 from boa.blockchain.vm.Neo.Storage import Get, Put, Delete, GetContext
 from boa.blockchain.vm.Neo.Runtime import Log, Notify
@@ -35,10 +43,11 @@ def Main(operation, args):
 
 def do_concat(args):
     """
+
     :param args: A list of arguments along with the operation
     :type args: list
     :return: result of combined values
-    :rtype: Union[bool, str]
+    :rtype: Union[bool, bytearray]
     """
     if len(args) > 1:
         a = args[0]
