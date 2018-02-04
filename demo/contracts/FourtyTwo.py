b0e6d830901464f190ea6fefb47b2afc78ab6d0e
Travis <rockacola@gmail.com>
2018-02-04 21:29:25 +1100

    Remove alt of 42 sample and add version control to 42 contract

--- demo/contracts/FourtyTwo.py
+++ demo/contracts/FourtyTwo.py
@@ -1,2 +1,14 @@
+
+"""
+Test Command:
+    build ./demo/contracts/FourtyTwo.py test ff 02 True True
+
+Import Command:
+    import contract ./demo/contracts/FourtyTwo.avm ff 02 True True
+
+Example Invocation:
+    testinvoke 51606190e821645403ff153d50c1a8e8320ac55a
+"""
+VERSION = 2
 def Main():
     return 42
