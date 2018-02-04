0aead895acf42c40269239f2fdf5c362e4561844
Travis <rockacola@gmail.com>
2018-02-05 01:55:20 +1100

    New implementation for NeoAlias

--- demo/contracts/projects/NeoAlias.py
+++ demo/contracts/projects/NeoAlias.py
@@ -47,6 +47,17 @@ def Main(operation: str, args: list) -> bytearray:
         elif operation == 'is_owner':       # Checking if invoker is the owner of the smart contract
             result = do_is_owner()
             return result
+        # Standard
+        elif operation == 'count_alias':    # Count numbers of alias assigned to the specified address
+            result = do_count_alias(args)
+            return result
+        elif operation == 'set_alias':      # Append a new alias to the specified address
+            result = do_set_alias(args)
+            return result
+        elif operation == 'get_alias':      # Fetch alias name to the specified address of a specified index
+            result = do_get_alias(args)
+            return result
+
         Log('unknown operation')
         return False
 
@@ -70,7 +81,78 @@ def do_is_owner() -> bool:
 # -- Standard operations
 
 
+def do_count_alias(args: list) -> int:
+    # Log('do_count_alias triggered.')
+    # Log('args:')
+    # Log(args)
+    if len(args) > 0:
+        context = GetContext()
+        address = args[0]
+        result = get_alias_count(context, address)
+        return result
+    Notify('invalid argument length')
+    return False
+
+
+def do_set_alias(args: list) -> bool:
+    if len(args) > 0:
+        context = GetContext()
+        address = args[0]
+        new_alias = args[1]
+        result = append_alias(context, address, new_alias)
+        return result
+    Notify('invalid argument length')
+    return False
+
+
+def do_get_alias(args: list) -> str:
+    if len(args) > 1:
+        context = GetContext()
+        address = args[0]
+        index = args[1]
+        result = get_alias(context, address, index)
+        return result
+    Notify('invalid argument length')
+    return False
+
 # -- Concrete methods
+
+
+def get_alias_count(context, address: str) -> int:
+    # TODO: validate address
+    key = concat(address, '_count')
+    # Log('key:')
+    # Log(key)
+    value = Get(context, key)
+    value += 0  # trick value to always be an integer
+    return value
+
+
+def set_alias_count(context, address: str, count: int) -> bool:
+    key = concat(address, '_count')
+    Put(context, key, count)
+    return True
+
+
+def append_alias(context, address: str, new_alias: str) -> bool:
+    # TODO: validate address
+    # TODO: validate alias
+    count = get_alias_count(context, address)
+    index = count
+    key = concat(address, '_')
+    key = concat(key, index)    # So you get "{addr}_{index}" as key
+    Put(context, key, new_alias)
+    set_alias_count(context, address, index)
+    return True
+
+
+def get_alias(context, address: str, index: int) -> str:
+    key = concat(address, '_')
+    key = concat(key, index)    # So you get "{addr}_{index}" as key
+    alias = Get(context, key)
+    Log('alias:')
+    Log(alias)
+    return alias
 
 
 # -- Helper methods
